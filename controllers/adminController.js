17e2fda5b22fb781d584ea2c7f333a86b897c09d
Munuri-Gangadhar <munurigangadhar0987@gmail.com>
2024-10-24 06:14:48 +0530

    changes

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -13,3 +13,18 @@ const adminLogin=(req,res)=>{
         res.status(401).json({message:'Invalid credentials provided'})
     }
 }
+
+
+const verifyAdmin = (req, res, next) => {
+    const token = req.header('Authorization').replace('Bearer ', '');
+    if (!token) return res.status(401).json({ message: 'No token provided' });
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.admin = decoded;
+        next();
+    } catch (error) {
+        res.status(403).json({ message: 'Invalid token' });
+    }
+};
+
+module.exports={adminLogin,verifyAdmin};
